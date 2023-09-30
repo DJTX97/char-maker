62a6bfd334319d38f42a61f4db4ee66ffed28000
DJTX97 <the.10.of.one@gmail.com>
2023-09-30 18:28:02 +0300

    feat: created core layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 
 export default function App() {
   return (
-    <div>
-      Hello!
+    <div className="h-screen bg-purple-300">
+      <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+  return (
+    <header className="py-10 text-center text-6xl ">
+      Welcome to AI Char Maker!
+    </header>
+  )
+}
