c82c0ce3f3c457a293ee123f5af9aec5f11401eb
DJTX97 <the.10.of.one@gmail.com>
2023-10-01 15:14:16 +0300

    feat: updated schema

--- src/interfaces/CharCardSchema.ts
+++ src/interfaces/CharCardSchema.ts
@@ -2,21 +2,21 @@ interface CharCardSchema {
     spec: string,
     spec_version: string,
     data: {
-      avatar: string,
+      avatar?: string,
       name: string,
       description: string,
-      personality: string,
-      mes_example: string,
-      scenario: string,
-      first_mes: string,
-      alternate_greetings: string[],
-      character_book: {
+      personality?: string,
+      mes_example?: string,
+      scenario?: string,
+      first_mes?: string,
+      alternate_greetings?: string[],
+      character_book?: {
         entries: [
           {
             id: number,
             keys: string[],
-            secondary_keys: string[],
-            comment: string,
+            secondary_keys?: string[],
+            comment?: string,
             content:string,
             constant: boolean,
             selective: boolean,
@@ -34,12 +34,12 @@ interface CharCardSchema {
         ],
         name: string
       },
-      creator: string,
-      character_version: string,
-      tags: string[],
-      creator_notes: string,
-      system_prompt: string,
-      post_history_instructions: string,
-      extensions: {}
+      creator?: string,
+      character_version?: string,
+      tags?: string[],
+      creator_notes?: string,
+      system_prompt?: string,
+      post_history_instructions?: string,
+      extensions?: {}
     }
   }
