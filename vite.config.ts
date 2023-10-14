0103acfa282e2c1599b328071917ae1939fa1e3a
DJTX97 <the.10.of.one@gmail.com>
2023-10-14 11:27:38 +0300

    fix: fixed production errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Group modules from node_modules into a separate 'vendor' chunk
+          }
+        },
+      },
+    },
+  },
+});
